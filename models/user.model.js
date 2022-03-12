3428728959cd950078b45d81abd852e3be6ddc46
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-03-12 15:42:17 +0500

    Model Update

--- models/user.model.js
+++ models/user.model.js
@@ -2,15 +2,40 @@ const mongoose = require('mongoose');
 const User = mongoose.model(
   'User',
   new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Role',
-      },
-    ],
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      uniqueCaseInsensitive: true,
+    },
+    isVerified: { type: Boolean, required: true, default: false },
+    password: { type: String, required: true },
+    phoneNumber: {
+      type: String,
+      required: false,
+      uniqueCaseInsensitive: true,
+    },
+    dateOfBirth: { type: Date, required: false },
+    gender: { type: String, required: false },
+    course: { type: String, required: false },
+    image: {
+      type: String,
+      default: `${process.env.DOMAIN}/images/default/default-avatar.png`,
+      required: false,
+    },
+    bookedTable: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Table',
+      required: false,
+    },
+    role: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Role',
+      default: 'member',
+    },
   })
 );
 module.exports = User;
